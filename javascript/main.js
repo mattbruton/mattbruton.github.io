4524ebe629f90af78d37083276336a6c299d62a2
MATT BRUTON <the.bruton@gmail.com>
2016-04-19 00:40:39 -0500

    added links to repo and live demo for html blog

--- javascript/main.js
+++ javascript/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   function displayRepos() {
     repositories.forEach(function(i){
       if (i.category == "NSS Exercises") {
-      $("#exercises").append(`<div class="repo col-md-4 col-sm-6"><h5>${i.title}</h5><div class="repoInfo"></div></div>`)
+      $("#exercises").append(`<div class="repo col-md-4 col-sm-6"><h5>${i.title}</h5><div class="repoInfo"><a href="${i.url}" class="repoUrl">Repository</a><a href="${i.live}" class="demoUrl">Live Demo</a></div></div>`)
       }
     });
   }
